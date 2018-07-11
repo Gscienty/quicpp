129556fe6b405e1aeb4c1e3a06956b2115e56fe6
Gscienty <gaoxiaochuan@hotmail.com>
2018-07-12 00:48:07 +0800

    implement varint class

---
+++ include/base/varint.h
@@ -0,0 +1,34 @@
+#ifndef _QUICPP_VARINT_
+#define _QUICPP_VARINT_
+
+#include <unistd.h>
+#include <stdint.h>
+#include <string>
+#include <istream>
+
+namespace quicpp {
+namespace base {
+    const size_t VARINT_LENGTH_1 = 1;
+    const size_t VARINT_LENGTH_2 = 2;
+    const size_t VARINT_LENGTH_4 = 4;
+    const size_t VARINT_LENGTH_8 = 8;
+
+    const uint64_t VARINT_MAX_1 = 63;
+    const uint64_t VARINT_MAX_2 = 16383;
+    const uint64_t VARINT_MAX_4 = 1073741823;
+    const uint64_t VARINT_MAX_8 = 4611686018427387903;
+
+    class varint {
+    private:
+        uint64_t value;
+    public:
+        varint(std::basic_istream<uint8_t> &buf);
+        varint(uint64_t &&);
+        size_t size() const;
+        std::basic_string<uint8_t> encode() const;
+    };
+}
+}
+
+#endif
+
---
+++ src/base/varint.cc
@@ -0,0 +1,86 @@
+#include "base/varint.h"
+
+quicpp::base::varint::varint(uint64_t &&value)
+    : value(value) { }
+
+quicpp::base::varint::varint(std::basic_istream<uint8_t> &buf) {
+    uint8_t first_byte = buf.peek();
+    uint8_t type = first_byte >> 6;
+    size_t size = 0;
+    switch (type) {
+        case 0x00:
+            // 1 bytes
+            this->value = first_byte & 0x3F;
+            size = 1;
+            break;
+        case 0x01:
+            // 2 bytes
+            size = 2;
+            reinterpret_cast<uint8_t *>(&this->value)[size - 1] = first_byte & 0x3F;
+            break;
+        case 0x02:
+            // 4 bytes
+            size = 4;
+            reinterpret_cast<uint8_t *>(&this->value)[size - 1] = first_byte & 0x3F;
+            break;
+        case 0x03:
+            // 8 bytes
+            size = 8;
+            reinterpret_cast<uint8_t *>(&this->value)[size - 1] = first_byte & 0x3F;
+            break;
+    }
+
+    if (size == 0) { return ; }
+    
+    for (size_t i = 1; i < size; i++) {
+        reinterpret_cast<uint8_t *>(&this->value)[size - 1 - i] = buf.get();
+    }
+}
+
+size_t quicpp::base::varint::size() const {
+    if (this->value < quicpp::base::VARINT_MAX_1) {
+        return quicpp::base::VARINT_LENGTH_1;
+    }
+    else if (this->value < quicpp::base::VARINT_MAX_2) {
+        return quicpp::base::VARINT_LENGTH_2;
+    }
+    else if (this->value < quicpp::base::VARINT_MAX_4) {
+        return quicpp::base::VARINT_LENGTH_4;
+    }
+    else if (this->value < quicpp::base::VARINT_MAX_8) {
+        return quicpp::base::VARINT_LENGTH_8;
+    }
+    else {
+        return 0;
+    }
+}
+
+std::basic_string<uint8_t> quicpp::base::varint::encode() const {
+    std::basic_string<uint8_t> ret;
+
+    size_t size = this->size();
+    // host byte order: little endian
+    // network byte order: big endian
+    // transform host byte order into network byte order
+    for (size_t i = 0; i < size; i++) {
+        ret.push_back(reinterpret_cast<const uint8_t *>(&this->value)[size - 1 - i]);
+    }
+
+    // append prefix at first byte
+    switch (size) {
+        case quicpp::base::VARINT_LENGTH_1:
+            ret[0] |= 0x00;
+            break;
+        case quicpp::base::VARINT_LENGTH_2:
+            ret[0] |= 0x40;
+            break;
+        case quicpp::base::VARINT_LENGTH_4:
+            ret[0] |= 0x80;
+            break;
+        case quicpp::base::VARINT_LENGTH_8:
+            ret[0] |= 0xC0;
+            break;
+    }
+
+    return ret;
+}
