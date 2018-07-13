d736a4c1ced9485aa0bdc607a9f0b35933fecb8f
Gscienty <gaoxiaochuan@hotmail.com>
2018-07-13 23:55:45 +0800

    add bigendian/littleendian encode

--- include/encodable.h
+++ include/encodable.h
@@ -10,6 +10,24 @@ namespace quicpp {
         virtual size_t size() const = 0;
         virtual void encode(std::basic_ostream<uint8_t> &out) const = 0;
     };
+
+    template <typename T>
+    static void bigendian_encode(std::basic_ostream<uint8_t> &out, const T value) {
+        size_t size = sizeof(T);
+
+        for (size_t off = 0; off < size; off++) {
+            out.put(reinterpret_cast<const uint8_t *>(&value)[size - 1 - off]);
+        }
+    }
+
+    template <typename T>
+    static void littleendian_encode(std::basic_ostream<uint8_t> &out, const T value) {
+        size_t size = sizeof(T);
+
+        for (size_t off = 0; off < size; off++) {
+            out.put(reinterpret_cast<const uint8_t *>(&value)[off]);
+        }
+    }
 }
 
 #endif
