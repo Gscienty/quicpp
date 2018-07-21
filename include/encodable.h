fae58346644d3c91dd343f1b09247699cbf57834
Gscienty <gaoxiaochuan@hotmail.com>
2018-07-22 03:48:49 +0800

    add decode

--- include/encodable.h
+++ include/encodable.h
@@ -2,6 +2,7 @@
 #define _QUICPP_ENCODABLE_
 
 #include <ostream>
+#include <istream>
 #include <stdint.h>
 
 namespace quicpp {
@@ -21,12 +22,34 @@ namespace quicpp {
     }
 
     template <typename T>
+    static T bigendian_decode(std::basic_istream<uint8_t> &in) {
+        size_t size = sizeof(T);
+
+        T result = 0;
+        for (size_t off = 0; off < size; off++) {
+            reinterpret_cast<uint8_t *>(&result)[size - 1 - off] = in.get();
+        }
+        return result;
+    }
+
+    template <typename T>
     static void littleendian_encode(std::basic_ostream<uint8_t> &out, const T value) {
         size_t size = sizeof(T);
 
         for (size_t off = 0; off < size; off++) {
             out.put(reinterpret_cast<const uint8_t *>(&value)[off]);
         }
+    }
+
+    template <typename T>
+    static T littleendian_decode(std::basic_istream<uint8_t> &in) {
+        size_t size = sizeof(T);
+
+        T result = 0;
+        for (size_t off = 0; off < size; off++) {
+            reinterpret_cast<uint8_t *>(&result)[off] = in.get();
+        }
+        return result;
     }
 }
 
