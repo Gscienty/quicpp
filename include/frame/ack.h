9afaacad575dcc65f7d1ac1f0991afe317503b9e
Gscienty <gaoxiaochuan@hotmail.com>
2018-07-28 03:23:48 +0800

    add ack frame

---
+++ include/frame/ack.h
@@ -0,0 +1,33 @@
+#ifndef _QUICPP_FRAME_ACK_
+#define _QUICPP_FRAME_ACK_
+
+#include "frame/type.h"
+#include <chrono>
+#include <utility>
+#include <list>
+
+namespace quicpp {
+namespace frame {
+
+const int ack_range_largest = 1;
+const int ack_range_smallest = 0;
+
+class ack : public quicpp::frame::frame {
+private:
+    std::chrono::duration<uint64_t, std::chrono::nanoseconds> delay;
+    std::list<std::pair<uint64_t, uint64_t>> ranges;
+public:
+    ack(std::basic_istream<uint8_t> &in);
+
+    virtual uint8_t get_type() const override;
+    virtual size_t size() const override;
+    virtual void encode(std::basic_ostream<uint8_t> &out) const override;
+
+    uint64_t largest() const;
+    uint64_t smallest() const;
+};
+
+}
+}
+
+#endif
---
+++ src/frame/ack.cc
@@ -0,0 +1,92 @@
+#include "frame/ack.h"
+#include "base/varint.h"
+#include <algorithm>
+
+quicpp::frame::ack::ack(std::basic_istream<uint8_t> &in) {
+    in.seekg(1, std::ios_base::cur);
+
+    quicpp::base::varint largest(in);
+    this->delay = quicpp::base::varint(in);
+    int len = uint64_t(quicpp::base::varint(in));
+
+    quicpp::base::varint first(in);
+    uint64_t top = uint64_t(largest) - uint64_t(first);
+    this->ranges.push_back(std::make_pair(top, uint64_t(largest)));
+
+    while (len--) {
+        quicpp::base::varint gap(in);
+        quicpp::base::varint len(in);
+
+        top -= uint64_t(gap);
+        this->ranges.push_back(std::make_pair(top - uint64_t(len), top));
+        top -= uint64_t(len);
+    }
+}
+
+uint8_t quicpp::frame::ack::get_type() const {
+    return quicpp::frame::frame_type_ack;
+}
+
+size_t quicpp::frame::ack::size() const {
+    size_t result = 1 +
+        quicpp::base::varint(this->largest()).size() + 
+        quicpp::base::varint(this->delay.count()).size() +
+        quicpp::base::varint(this->ranges.size() - 1).size();
+    
+    auto begin = this->ranges.begin();
+    uint64_t largest = std::get<quicpp::frame::ack_range_largest>(*begin);
+    uint64_t smallest = std::get<quicpp::frame::ack_range_smallest>(*begin);
+    result += quicpp::base::varint(largest - smallest).size();
+
+    begin++;
+    std::for_each(begin, this->ranges.end(),
+                  [&] (const std::pair<uint64_t, uint64_t> &range) -> void {
+
+                    uint64_t len = std::get<quicpp::frame::ack_range_largest>(range) - 
+                        std::get<quicpp::frame::ack_range_smallest>(range);
+                    uint64_t gap = smallest - std::get<quicpp::frame::ack_range_largest>(range);
+
+                    largest = std::get<quicpp::frame::ack_range_largest>(range);
+                    smallest = std::get<quicpp::frame::ack_range_smallest>(range);
+
+                    result += quicpp::base::varint(gap).size();
+                    result += quicpp::base::varint(len).size();
+
+                  });
+
+    return result;
+}
+
+void quicpp::frame::ack::encode(std::basic_ostream<uint8_t> &out) const {
+    out.put(this->get_type());
+    quicpp::base::varint(this->largest()).encode(out);
+    quicpp::base::varint(this->delay.count()).encode(out);
+    quicpp::base::varint(this->ranges.size()).encode(out);
+
+    auto begin = this->ranges.begin();
+    uint64_t smallest = std::get<quicpp::frame::ack_range_smallest>(*begin);
+    quicpp::base::varint(std::get<quicpp::frame::ack_range_largest>(*begin) - smallest).encode(out);
+
+    begin++;
+    std::for_each(begin, this->ranges.end(),
+                  [&] (const std::pair<uint64_t, uint64_t> &range) -> void {
+                    
+                    uint64_t len = std::get<quicpp::frame::ack_range_largest>(range) - 
+                        std::get<quicpp::frame::ack_range_smallest>(range);
+                    uint64_t gap = smallest - std::get<quicpp::frame::ack_range_largest>(range);
+
+                    smallest = std::get<quicpp::frame::ack_range_smallest>(range);
+
+                    quicpp::base::varint(gap).encode(out);
+                    quicpp::base::varint(len).encode(out);
+
+                  });
+}
+
+uint64_t quicpp::frame::ack::largest() const {
+    return std::get<quicpp::frame::ack_range_largest>(*this->ranges.begin());
+}
+
+uint64_t quicpp::frame::ack::smallest() const {
+    return std::get<quicpp::frame::ack_range_smallest>(*this->ranges.rbegin());
+}
