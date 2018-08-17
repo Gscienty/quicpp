9cdc40a305fd9ec535ce5b66ddcaeb89c33439cd
高小川 <gaoxiaochuan@hotmail.com>
2018-08-17 18:42:50 +0800

    add default tcp mss

--- include/params.h
+++ include/params.h
@@ -1,10 +1,13 @@
 #ifndef _QUICPP_PARAMS_
 #define _QUICPP_PARAMS_
 
+#include <cstdint>
+
 namespace quicpp {
 
 const double window_update_threshole = 0.25;
 const double connection_flowcontrol_multiplier = 1.5;
+const uint64_t default_tcp_mss = 1460;
 
 }
 
