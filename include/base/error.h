2f623d342495f2a64d28d98ae1100d649f692780
高小川 <gaoxiaochuan@hotmail.com>
2018-08-17 18:42:19 +0800

    add packet error

--- include/base/error.h
+++ include/base/error.h
@@ -10,6 +10,7 @@ class error_t {
 private:
     uint64_t sign;
 public:
+    error_t() : sign(0) {}
     error_t(uint64_t sign) : sign(sign) {}
 
     bool operator== (const quicpp::base::error_t &err) {
@@ -29,6 +30,8 @@ namespace error {
 const quicpp::base::error_t success(0);
 const quicpp::base::error_t flowcontrol_recv_too_much_data(1000);
 const quicpp::base::error_t stream_data_after_termination(1001);
+const quicpp::base::error_t packet_not_found(2000);
+const quicpp::base::error_t num_outstanding_handshake_packets_negative(2001);
 
 }
 }
