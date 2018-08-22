9d334e52e0d560ec7c72110be90766064f87874b
高小川 <gaoxiaochuan@hotmail.com>
2018-08-23 01:50:12 +0800

    add abstract send_algo

---
+++ include/congestion/send_algo.h
@@ -0,0 +1,38 @@
+#ifndef _QUICPP_CONGESTION_SEND_ALGO_
+#define _QUICPP_CONGESTION_SEND_ALGO_
+
+#include <chrono>
+
+namespace quicpp {
+namespace congestion {
+
+class send_algo {
+    virtual
+    std::chrono::microseconds 
+    time_until_send(const uint64_t bytes_inflight) = 0;
+    virtual
+    void 
+    on_packet_sent(const uint64_t pn,
+                   const uint64_t bytes,
+                   const bool is_retransmittable) = 0;
+
+    virtual uint64_t &cwnd() = 0;
+
+    virtual void maybe_exit_slowstart() = 0;
+
+    virtual
+    void
+    on_packet_acked(const uint64_t acked_pn,
+                    const uint64_t acked_bytes,
+                    const uint64_t prior_inflight,
+                    const std::chrono::system_clock::time_point event_time) = 0;
+
+    virtual
+    void 
+    on_packet_lost(const uint64_t pn,
+                   const uint64_t lost_bytes,
+                   const uint64_t prior_inflight) = 0;
+};
+}
+
+#endif
