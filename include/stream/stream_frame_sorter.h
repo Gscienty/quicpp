ac75254fdea27cfd9625c22392cd4a9e8660df4a
高小川 <gaoxiaochuan@hotmail.com>
2018-09-13 02:12:58 +0800

    add stream frame sorter

---
+++ include/stream/stream_frame_sorter.h
@@ -0,0 +1,29 @@
+#ifndef _QUICPP_STREAM_STREAM_FRAME_SORTER_
+#define _QUICPP_STREAM_STREAM_FRAME_SORTER_
+
+#include "frame/stream.h"
+#include "base/error.h"
+#include <cstdint>
+#include <map>
+#include <list>
+#include <utility>
+
+namespace quicpp {
+namespace stream {
+
+class stream_frame_sorter {
+private:
+    std::map<uint64_t, quicpp::frame::stream *> queued_frames;
+    uint64_t read_position;
+    std::list<std::pair<uint64_t, uint64_t>> gaps;
+public:
+    stream_frame_sorter();
+    quicpp::base::error_t push(quicpp::frame::stream *frame);
+    quicpp::frame::stream *pop();
+    quicpp::frame::stream *head();
+};
+
+}
+}
+
+#endif
---
+++ src/stream/stream_frame_sorter.cc
@@ -0,0 +1,140 @@
+#include "stream/stream_frame_sorter.h"
+#include "params.h"
+
+quicpp::stream::stream_frame_sorter::stream_frame_sorter()
+    : read_position(0) {
+    this->gaps.push_back(std::make_pair(0UL, quicpp::max_byte_count));
+}
+
+quicpp::base::error_t
+quicpp::stream::stream_frame_sorter::push(quicpp::frame::stream *frame) {
+    if (frame->empty()) {
+        if (frame->final_flag()) {
+            this->queued_frames[frame->offset()] = frame;
+        }
+        return quicpp::error::success;
+    }
+
+    bool was_cut = false;
+    auto old_frame_itr = this->queued_frames.find(frame->offset());
+    if (old_frame_itr != this->queued_frames.end()) {
+        if (frame->len() <= old_frame_itr->second->len()) {
+            return quicpp::error::duplicate_stream_data;
+        }
+        frame->data() =
+            std::basic_string<uint8_t>(frame->data().begin() + old_frame_itr->second->len(),
+                                       frame->data().end());
+        frame->offset() = frame->offset() + old_frame_itr->second->len();
+        was_cut = true;
+    }
+
+    uint64_t start = frame->offset();
+    uint64_t end = frame->offset() + frame->len();
+    
+    auto gap_itr = this->gaps.begin();
+    for (;
+         gap_itr != this->gaps.end();
+         gap_itr++) {
+        if (end <= std::get<0>(*gap_itr)) {
+            return quicpp::error::duplicate_stream_data;
+        }
+        if (end > std::get<0>(*gap_itr) && start <= std::get<1>(*gap_itr)) {
+            break;
+        }
+    }
+
+    if (gap_itr == this->gaps.end()) {
+        return quicpp::error::bug;
+    }
+
+    if (start < std::get<0>(*gap_itr)) {
+        uint64_t add = std::get<0>(*gap_itr) - start;
+        frame->offset() = frame->offset() + add;
+        frame->data() = std::basic_string<uint8_t>(frame->data().begin() + add,
+                                                   frame->data().end());
+        was_cut = true;
+    }
+
+    auto endgap_itr = gap_itr;
+    while (end >= std::get<1>(*endgap_itr)) {
+        auto next_endgap = endgap_itr;
+        next_endgap++;
+
+        if (next_endgap == this->gaps.end()) {
+            return quicpp::error::bug;
+        }
+        if (endgap_itr != gap_itr) {
+            this->gaps.erase(endgap_itr);
+        }
+        if (end <= std::get<0>(*next_endgap)) {
+            break;
+        }
+        this->queued_frames
+            .erase(this->queued_frames.find(std::get<1>(*endgap_itr)));
+        endgap_itr = next_endgap;
+    }
+
+    if (end > std::get<1>(*endgap_itr)) {
+        uint64_t cut_len = end - std::get<1>(*endgap_itr);
+        uint64_t len = frame->len() - cut_len;
+        end -= cut_len;
+        frame->data() =
+            std::basic_string<uint8_t>(frame->data().begin(),
+                                       frame->data().begin() + len);
+        was_cut = true;
+    }
+
+    if (start == std::get<0>(*gap_itr)) {
+        if (end >= std::get<1>(*gap_itr)) {
+            this->gaps.erase(gap_itr);
+        }
+        if (end < std::get<1>(*endgap_itr)) {
+            std::get<0>(*endgap_itr) = end;
+        }
+    }
+    else if (end == std::get<1>(*endgap_itr)) {
+        std::get<1>(*gap_itr) = start;
+    }
+    else {
+        if (gap_itr == endgap_itr) {
+            this->gaps.insert(gap_itr,
+                              std::make_pair(end, std::get<1>(*gap_itr)));
+            std::get<1>(*gap_itr) = start;
+        }
+        else {
+            std::get<1>(*gap_itr) = start;
+            std::get<0>(*endgap_itr) = end;
+        }
+    }
+
+    if (this->gaps.size() > quicpp::max_stream_frame_sorter_gaps) {
+        return quicpp::error::too_many_gaps_in_received_stream_data;
+    }
+
+    if (was_cut) {
+        frame->data() =
+            std::basic_string<uint8_t>(frame->data().begin(),
+                                       frame->data().begin() + frame->len());
+    }
+
+    this->queued_frames[frame->offset()] = frame;
+
+    return quicpp::error::success;
+}
+
+quicpp::frame::stream *quicpp::stream::stream_frame_sorter::pop() {
+    quicpp::frame::stream *frame = this->head();
+    if (frame != nullptr) {
+        this->read_position += frame->len();
+        this->queued_frames.erase(this->queued_frames.find(frame->offset()));
+    }
+    return frame;
+}
+
+quicpp::frame::stream *quicpp::stream::stream_frame_sorter::head() {
+    auto frame_itr = this->queued_frames.find(this->read_position);
+    if (frame_itr != this->queued_frames.end()) {
+        return std::get<1>(*frame_itr);
+    }
+    return nullptr;
+}
