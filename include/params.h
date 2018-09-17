f9d470520f734b8f0292aa02fee8d04aff101f28
高小川 <gaoxiaochuan@hotmail.com>
2018-09-18 01:57:55 +0800

    add param

--- include/params.h
+++ include/params.h
@@ -19,6 +19,7 @@ const std::chrono::microseconds min_pacing_delay(100);
 const int max_tracked_received_ack_ranges = default_max_cwnd_packets;
 const uint64_t max_byte_count = (1UL << 62) - 1;
 const int max_stream_frame_sorter_gaps = 1000;
+const uint64_t min_stream_frame_size = 128;
 
 
 }
