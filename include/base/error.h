4e2900d9c0f33a4ae14fbd549d2832a2d1ca2498
高小川 <gaoxiaochuan@hotmail.com>
2018-09-18 15:34:42 +0800

    add error

--- include/base/error.h
+++ include/base/error.h
@@ -45,6 +45,8 @@ const quicpp::base::error_t stream_was_reset(4003);
 const quicpp::base::error_t close_canceled_stream(4004);
 const quicpp::base::error_t write_on_canceled_stream(4005);
 const quicpp::base::error_t write_on_closed_stream(4006);
+const quicpp::base::error_t peer_tried_to_open_stream(4007);
+const quicpp::base::error_t tried_to_delete_unknow_stream(4008);
 
 }
 }
