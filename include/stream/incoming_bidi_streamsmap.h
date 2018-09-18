730362c5aabdaeba05449158528d2b151d5b3bb1
高小川 <gaoxiaochuan@hotmail.com>
2018-09-18 15:35:12 +0800

    add incoming bidi streams map

---
+++ include/stream/incoming_bidi_streamsmap.h
@@ -0,0 +1,46 @@
+#ifndef _QUICPP_STREAM_INCOMING_BIDI_STREAMS_MAP_
+#define _QUICPP_STREAM_INCOMING_BIDI_STREAMS_MAP_
+
+#include "stream/stream.h"
+#include "base/stream_id_t.h"
+#include "base/error.h"
+#include "frame/max_stream_id.h"
+#include "rw_mutex.h"
+#include <map>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+namespace quicpp {
+namespace stream {
+
+class incoming_bidi_streamsmap {
+private:
+    quicpp::rw_mutex rw_mutex;
+    std::condition_variable cond;
+    std::map<quicpp::base::stream_id_t, quicpp::stream::stream *> streams;
+    quicpp::base::stream_id_t next_stream;
+    quicpp::base::stream_id_t highest_stream;
+    quicpp::base::stream_id_t max_stream;
+    int max_num_streams;
+    std::function<quicpp::stream::stream * (quicpp::base::stream_id_t)> new_stream;
+    std::function<void (quicpp::frame::frame *)> _queue_max_stream_id;
+    std::function<void (quicpp::frame::max_stream_id *)> queue_max_stream_id;
+    quicpp::base::error_t close_err;
+public:
+    incoming_bidi_streamsmap(quicpp::base::stream_id_t next_stream,
+                             quicpp::base::stream_id_t initial_max_stream_id,
+                             int max_num_streams,
+                             std::function<quicpp::stream::stream * (quicpp::base::stream_id_t)> new_stream,
+                             std::function<void (quicpp::frame::frame *)> queue_max_stream_id);
+    std::pair<quicpp::stream::stream *, quicpp::base::error_t> accept_stream();
+    std::pair<quicpp::stream::stream *, quicpp::base::error_t>
+    get_or_open_stream(quicpp::base::stream_id_t id);
+    quicpp::base::error_t delete_stream(quicpp::base::stream_id_t id);
+    void close_with_error(quicpp::base::error_t err);
+};
+
+}
+}
+
+#endif
---
+++ src/stream/incoming_bidi_streamsmap.cc
@@ -0,0 +1,114 @@
+#include "stream/incoming_bidi_streamsmap.h"
+#include "frame/max_stream_id.h"
+
+
+quicpp::stream::incoming_bidi_streamsmap::
+incoming_bidi_streamsmap(quicpp::base::stream_id_t next_stream,
+                         quicpp::base::stream_id_t initial_max_stream_id,
+                         int max_num_streams,
+                         std::function<quicpp::stream::stream * (quicpp::base::stream_id_t)> new_stream,
+                         std::function<void (quicpp::frame::frame *)> queue_max_stream_id)
+    : next_stream(next_stream)
+    , max_stream(initial_max_stream_id)
+    , max_num_streams(max_num_streams)
+    , new_stream(new_stream)
+    , _queue_max_stream_id(queue_max_stream_id)
+    , queue_max_stream_id([this] (quicpp::frame::max_stream_id *frame) -> void {
+                            this->_queue_max_stream_id(frame);
+                          })
+    , close_err(quicpp::error::success) {}
+
+std::pair<quicpp::stream::stream *, quicpp::base::error_t>
+quicpp::stream::incoming_bidi_streamsmap::accept_stream() {
+    std::unique_lock<std::mutex> locker(this->rw_mutex.mutex());
+
+    quicpp::stream::stream *ret_str = nullptr;
+
+    while (true) {
+        if (this->close_err != quicpp::error::success) {
+            return std::make_pair(nullptr, this->close_err);
+        }
+        auto ret_str_itr = this->streams.find(this->next_stream);
+        if (ret_str_itr != this->streams.end()) {
+            ret_str = ret_str_itr->second;
+            break;
+        }
+        this->cond.wait(locker);
+    }
+    this->next_stream += 4;
+    return std::make_pair(ret_str, quicpp::error::success);
+}
+
+std::pair<quicpp::stream::stream *, quicpp::base::error_t>
+quicpp::stream::incoming_bidi_streamsmap::
+get_or_open_stream(quicpp::base::stream_id_t id) {
+    {
+        quicpp::reader_lock_guard locker(this->rw_mutex);
+
+        if (id > this->max_stream) {
+            return std::make_pair(nullptr,
+                                  quicpp::error::peer_tried_to_open_stream);
+        }
+
+        if (id <= this->highest_stream) {
+            return std::make_pair(this->streams[id], quicpp::error::success);
+        }
+    }
+
+    {
+        quicpp::writer_lock_guard locker(this->rw_mutex);
+        quicpp::base::stream_id_t start;
+        if (this->highest_stream == 0) {
+            start = this->next_stream;
+        }
+        else {
+            start = this->highest_stream + 4;
+        }
+
+        for (quicpp::base::stream_id_t new_id = start;
+             new_id <= id;
+             new_id += 4) {
+            this->streams[new_id] = this->new_stream(new_id);
+            this->cond.notify_one();
+        }
+        this->highest_stream = id;
+        return std::make_pair(this->streams[id], quicpp::error::success);
+    }
+}
+
+quicpp::base::error_t
+quicpp::stream::incoming_bidi_streamsmap::
+delete_stream(quicpp::base::stream_id_t id) {
+    quicpp::writer_lock_guard locker(this->rw_mutex);
+
+    if (this->streams.find(id) == this->streams.end()) {
+        return quicpp::error::tried_to_delete_unknow_stream;
+    }
+    this->streams.erase(this->streams.find(id));
+
+    int num_new_streams = this->max_num_streams - this->streams.size();
+    if (num_new_streams > 0) {
+        this->max_stream = this->highest_stream + 4 * num_new_streams;
+        quicpp::frame::max_stream_id *frame =
+            new quicpp::frame::max_stream_id();        
+        frame->maximum_stream_id() = this->max_stream;
+        this->queue_max_stream_id(frame);
+    }
+
+    return quicpp::error::success;
+}
+
+void quicpp::stream::incoming_bidi_streamsmap::
+close_with_error(quicpp::base::error_t err) {
+    {
+        quicpp::writer_lock_guard locker(this->rw_mutex);
+        this->close_err = err;
+        for (auto str_itr = this->streams.begin();
+             str_itr != this->streams.end();
+             str_itr++) {
+            str_itr->second->close_for_shutdown(err);
+        }
+    }
+    this->cond.notify_all();
+    
+}
