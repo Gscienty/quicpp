3bc5f7e7d9fbef7fb8400bf46499a051dc5ad9d5
高小川 <gaoxiaochuan@hotmail.com>
2018-09-20 02:33:11 +0800

    change readable/writable

--- include/base/readable.h
+++ include/base/readable.h
@@ -1,15 +1,19 @@
 #ifndef _QUICPP_BASE_READABLE_
 #define _QUICPP_BASE_READABLE_
 
+#include "base/error.h"
 #include <cstdint>
 #include <cstdlib>
+#include <utility>
 
 namespace quicpp {
 namespace base {
 
 class readable {
 public:
-    virtual ssize_t read(uint8_t *buffer_ptr, size_t size) = 0;
+    virtual
+    std::pair<ssize_t, quicpp::base::error_t>
+    read(uint8_t *buffer_ptr, size_t size) = 0;
 };
 
 }
--- include/base/writable.h
+++ include/base/writable.h
@@ -1,15 +1,19 @@
 #ifndef _QUICPP_BASE_WRITABLE_
 #define _QUICPP_BASE_WRITABLE_
 
+#include "base/error.h"
 #include <cstdint>
 #include <cstdlib>
+#include <utility>
 
 namespace quicpp {
 namespace base {
 
 class writable {
 public:
-    virtual int write(uint8_t *buffer_ptr, size_t size) = 0;
+    virtual
+    std::pair<ssize_t, quicpp::base::error_t>
+    write(uint8_t *buffer_ptr, size_t size) = 0;
 };
 
 }
