09fcce9aa3675946502abd5a480109e4acbd6f85
高小川 <gaoxiaochuan@hotmail.com>
2018-09-21 03:15:13 +0800

    modify fields name

--- include/packet/header.h
+++ include/packet/header.h
@@ -20,20 +20,30 @@ const uint8_t header_type_0RTT_protected = 0x7C;
 
 class header : public quicpp::encodable {
 private:
-    bool header_form;
-    bool key_phase;
-    uint8_t packet_type;
-    uint32_t version;
-    quicpp::base::conn_id dest_conn_id;
-    quicpp::base::conn_id src_conn_id;
-    quicpp::base::varint payload_length;
-    size_t packet_number_length;
-    uint32_t packet_number;
+    bool _header_form;
+    bool _key_phase;
+    uint8_t _type;
+    uint32_t _version;
+    quicpp::base::conn_id _dest_conn_id;
+    quicpp::base::conn_id _src_conn_id;
+    quicpp::base::varint _payload_length;
+    size_t _packet_number_length;
+    uint32_t _packet_number;
 
 public:
     header(std::basic_istream<uint8_t> &in, const size_t dest_conn_id_len = 0);
     virtual size_t size() const override;
     virtual void encode(std::basic_ostream<uint8_t> &out) const override;
+
+    bool &header_form() { return this->_header_form; }
+    bool &key_phase() { return this->_key_phase; }
+    uint8_t &type() { return this->_type; }
+    uint32_t &version() { return this->_version; }
+    quicpp::base::conn_id &dest_connid() { return this->_dest_conn_id; }
+    quicpp::base::conn_id &src_connid() { return this->_src_conn_id; }
+    quicpp::base::varint &payload_length() { return this->_payload_length; }
+    size_t &packet_number_length() { return this->_packet_number_length; }
+    uint32_t &packet_number() { return this->_packet_number; } 
 };
 
 }
--- src/packet/header.cc
+++ src/packet/header.cc
@@ -4,104 +4,104 @@ quicpp::packet::header::header(std::basic_istream<uint8_t> &in, const size_t des
     uint8_t first_byte = in.get();
     if ((first_byte & 0x80) != 0x00) {
         // long packet type
-        this->header_form = quicpp::packet::header_form_long_header;
+        this->_header_form = quicpp::packet::header_form_long_header;
         // packet type
-        this->packet_type = 0x7F & first_byte;
+        this->_type = 0x7F & first_byte;
         // version
-        this->version = quicpp::bigendian_decode<uint32_t>(in);
+        this->_version = quicpp::bigendian_decode<uint32_t>(in);
         // id length
         uint8_t id_length = in.get();
         size_t dest_conn_id_len = ((id_length & 0xF0) >> 4) + 3;
         size_t src_conn_id_len = ((id_length & 0x0F)) + 3;
         // destination connection id
-        this->dest_conn_id = quicpp::base::conn_id(in, dest_conn_id_len);
+        this->_dest_conn_id = quicpp::base::conn_id(in, dest_conn_id_len);
         // source connection id
-        this->src_conn_id = quicpp::base::conn_id(in, src_conn_id_len);
-        if (this->version == 0x00000000) {
+        this->_src_conn_id = quicpp::base::conn_id(in, src_conn_id_len);
+        if (this->_version == 0x00000000) {
             // version negotiation packet
             return;
         }
         // payload length
-        this->payload_length = quicpp::base::varint(in);
+        this->_payload_length = quicpp::base::varint(in);
         // packet number
-        this->packet_number = quicpp::bigendian_decode<uint32_t>(in);
+        this->_packet_number = quicpp::bigendian_decode<uint32_t>(in);
     }
     else {
         // short header
-        this->header_form = quicpp::packet::header_form_short_header;
+        this->_header_form = quicpp::packet::header_form_short_header;
         // key phase bit
-        this->key_phase = (first_byte & 0x40) != 0;
+        this->_key_phase = (first_byte & 0x40) != 0;
         // destinaction connection id
-        this->dest_conn_id = quicpp::base::conn_id(in, dest_conn_id_len);
-        switch ((this->packet_number & 0x03)) {
+        this->_dest_conn_id = quicpp::base::conn_id(in, dest_conn_id_len);
+        switch ((this->_packet_number & 0x03)) {
             case 0x00:
-                this->packet_number = uint32_t(quicpp::bigendian_decode<uint8_t>(in));
+                this->_packet_number = uint32_t(quicpp::bigendian_decode<uint8_t>(in));
                 break;
             case 0x01:
-                this->packet_number = uint32_t(quicpp::bigendian_decode<uint16_t>(in));
+                this->_packet_number = uint32_t(quicpp::bigendian_decode<uint16_t>(in));
                 break;
             case 0x02:
-                this->packet_number = uint32_t(quicpp::bigendian_decode<uint32_t>(in));
+                this->_packet_number = uint32_t(quicpp::bigendian_decode<uint32_t>(in));
                 break;
             default:
-                this->packet_number = 0;
+                this->_packet_number = 0;
         }
     }
 
 }
 
 size_t quicpp::packet::header::size() const {
-    if (this->header_form == quicpp::packet::header_form_long_header) {
-        size_t result = 1 + 4 + 1 + this->dest_conn_id.size() + this->src_conn_id.size();
-        if (this->version != 0x00000000) {
+    if (this->_header_form == quicpp::packet::header_form_long_header) {
+        size_t result = 1 + 4 + 1 + this->_dest_conn_id.size() + this->_src_conn_id.size();
+        if (this->_version != 0x00000000) {
             // version negotiation
-            result += this->payload_length.size() + this->packet_number_length;
+            result += this->_payload_length.size() + this->_packet_number_length;
         }
         return result;
     }
-    return 1 + this->dest_conn_id.size() + this->packet_number_length;
+    return 1 + this->_dest_conn_id.size() + this->_packet_number_length;
 }
 
 void quicpp::packet::header::encode(std::basic_ostream<uint8_t> &out) const {
-    if (this->header_form == quicpp::packet::header_form_long_header) {
+    if (this->_header_form == quicpp::packet::header_form_long_header) {
         // header form | long packet type
-        out.put(0x80 | this->packet_type);
+        out.put(0x80 | this->_type);
         // version
-        quicpp::bigendian_encode(out, this->version);
+        quicpp::bigendian_encode(out, this->_version);
         // id length
         uint8_t id_length = 0x00;
-        if (this->dest_conn_id.size() != 0) {
-            id_length = (this->dest_conn_id.size() - 3) << 4;
+        if (this->_dest_conn_id.size() != 0) {
+            id_length = (this->_dest_conn_id.size() - 3) << 4;
         }
-        if (this->src_conn_id.size() != 0) {
-            id_length += this->src_conn_id.size() - 3;
+        if (this->_src_conn_id.size() != 0) {
+            id_length += this->_src_conn_id.size() - 3;
         }
         out.put(id_length);
         // destination connection id
-        this->dest_conn_id.encode(out);
+        this->_dest_conn_id.encode(out);
         // source connection id
-        this->src_conn_id.encode(out);
-        if (this->version == 0x00000000) {
+        this->_src_conn_id.encode(out);
+        if (this->_version == 0x00000000) {
             // version negotiation
             return;
         }
         // payload length
-        this->payload_length.encode(out);
+        this->_payload_length.encode(out);
         // packet number
-        quicpp::bigendian_encode(out, this->packet_number);
+        quicpp::bigendian_encode(out, this->_packet_number);
     }
     else {
         // header form | short packet type
         uint8_t first_byte = 0x00;
-        if (this->key_phase) {
+        if (this->_key_phase) {
             first_byte |= 0x40;
         }
         first_byte |= 0x30;
-        if (this->packet_number < 0x100) {
+        if (this->_packet_number < 0x100) {
             // one byte
             first_byte |= 0x00;
         }
-        else if (this->packet_number < 0x10000) {
+        else if (this->_packet_number < 0x10000) {
             // two byte
             first_byte |= 0x01;
         }
@@ -110,16 +110,16 @@ void quicpp::packet::header::encode(std::basic_ostream<uint8_t> &out) const {
         }
         out.put(first_byte);
         // destination connection id
-        this->dest_conn_id.encode(out);
+        this->_dest_conn_id.encode(out);
         // packet number
-        if (this->packet_number < 0x100) {
-            quicpp::bigendian_encode(out, uint8_t(this->packet_number));
+        if (this->_packet_number < 0x100) {
+            quicpp::bigendian_encode(out, uint8_t(this->_packet_number));
         }
-        else if (this->packet_number < 0x10000) {
-            quicpp::bigendian_encode(out, uint16_t(this->packet_number));
+        else if (this->_packet_number < 0x10000) {
+            quicpp::bigendian_encode(out, uint16_t(this->_packet_number));
         }
         else {
-            quicpp::bigendian_encode(out, this->packet_number);
+            quicpp::bigendian_encode(out, this->_packet_number);
         }
     }
 }
