9cdbabd3c151d7bba4f81734b05ae209000ca17a
高小川 <gaoxiaochuan@hotmail.com>
2018-09-20 15:16:34 +0800

    add packet number generator

--- include/base/packet_number.h
+++ include/base/packet_number.h
@@ -1,8 +1,10 @@
 #ifndef _QUICPP_BASE_PACKET_NUMBER_
 #define _QUICPP_BASE_PACKET_NUMBER_
 
+#include "base/error.h"
 #include <cstdlib>
 #include <cstdint>
+#include <utility>
 
 namespace quicpp {
 namespace base {
@@ -11,6 +13,19 @@ size_t get_packet_number_length_for_header(uint64_t packet_number,
                                            uint64_t least_unacked);
 
 size_t get_packet_number_length(uint64_t packet_number);
+
+class packet_number_generator {
+private:
+    uint64_t average_period;
+    uint64_t _next;
+    uint64_t next_to_skip;
+public:
+    packet_number_generator(uint64_t initial, uint64_t average_period);
+    uint64_t peek() const;
+    uint64_t pop();
+    quicpp::base::error_t generate_new_skip();
+    uint16_t get_random_number();
+};
 
 }
 }
--- src/base/packet_number.cc
+++ src/base/packet_number.cc
@@ -1,10 +1,10 @@
 #include "base/packet_number.h"
+#include <random>
+#include <limits>
 
-namespace quicpp {
-namespace base {
-
-size_t get_packet_number_length_for_header(uint64_t packet_number,
-                                           uint64_t least_unacked) {
+size_t
+quicpp::base::get_packet_number_length_for_header(uint64_t packet_number,
+                                                  uint64_t least_unacked) {
     uint64_t diff = packet_number - least_unacked;
 
     if (diff < (1<<(14 - 1))) {
@@ -14,7 +14,7 @@ size_t get_packet_number_length_for_header(uint64_t packet_number,
     return 4;
 }
 
-size_t get_packet_number_length(uint64_t packet_number) {
+size_t quicpp::base::get_packet_number_length(uint64_t packet_number) {
     if (packet_number < (uint64_t(1) << (1 * 8))) {
         return 1;
     }
@@ -27,5 +27,39 @@ size_t get_packet_number_length(uint64_t packet_number) {
     return 6;
 }
 
+quicpp::base::packet_number_generator::
+packet_number_generator(uint64_t initial,
+                        uint64_t average_period)
+    : average_period(average_period)
+    , _next(initial)
+    , next_to_skip(0) {}
+
+uint64_t quicpp::base::packet_number_generator::peek() const {
+    return this->_next;
 }
+
+uint64_t quicpp::base::packet_number_generator::pop() {
+    uint64_t ret = this->_next;
+    
+    if (this->_next == this->next_to_skip) {
+        this->_next++;
+        this->generate_new_skip();
+    }
+
+    return ret;
+}
+
+quicpp::base::error_t quicpp::base::packet_number_generator::generate_new_skip() {
+    uint64_t skip = uint64_t(this->get_random_number()) *
+        (this->average_period - 1) /
+        (std::numeric_limits<uint16_t>::max() / 2);
+
+    this->next_to_skip = this->next_to_skip + 2 + skip;
+
+    return quicpp::error::success;
+}
+
+uint16_t
+quicpp::base::packet_number_generator::get_random_number() {
+    return std::rand() % 0x10000;
 }
