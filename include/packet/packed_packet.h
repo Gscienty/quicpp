4b22aac5f1feccdc9b1311efe28ba96c0ed554c0
高小川 <gaoxiaochuan@hotmail.com>
2018-09-23 02:15:50 +0800

    add packed packet getter/setter

--- include/packet/packed_packet.h
+++ include/packet/packed_packet.h
@@ -15,11 +15,16 @@ namespace packet {
 
 class packed_packet {
 private:
-    std::shared_ptr<quicpp::packet::header> header;
-    std::basic_string<uint8_t> raw;
-    std::vector<quicpp::frame::frame *> frames;
-    uint8_t encryption_level;
+    std::shared_ptr<quicpp::packet::header> _header;
+    std::basic_string<uint8_t> _raw;
+    std::vector<std::shared_ptr<quicpp::frame::frame>> _frames;
+    uint8_t _encryption_level;
 public:
+    packed_packet() : _encryption_level(0) {}
+    std::shared_ptr<quicpp::packet::header> &header() { return this->_header; }
+    std::basic_string<uint8_t> &raw() { return this->_raw; }
+    std::vector<std::shared_ptr<quicpp::frame::frame>> &frames() { return this->_frames; }
+    uint8_t &encryption_level() { return this->_encryption_level; }
     std::shared_ptr<quicpp::ackhandler::packet> to_ack_handler_packet();
 };
 
--- src/packet/packed_packet.cc
+++ src/packet/packed_packet.cc
@@ -4,11 +4,11 @@ std::shared_ptr<quicpp::ackhandler::packet>
 quicpp::packet::packed_packet::to_ack_handler_packet() {
     auto result = std::make_shared<quicpp::ackhandler::packet>();
 
-    result->packet_number = this->header->packet_number();
-    result->packet_type = this->header->type();
-    result->frames = this->frames;
-    result->len = this->raw.size();
-    result->encryption_level = this->encryption_level;
+    result->packet_number = this->_header->packet_number();
+    result->packet_type = this->_header->type();
+    result->frames = this->_frames;
+    result->len = this->_raw.size();
+    result->encryption_level = this->_encryption_level;
     result->send_time = std::chrono::system_clock::now();
 
     return result;
