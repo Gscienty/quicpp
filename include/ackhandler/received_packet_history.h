e3908d4348dcd31008fcabf7f8da9f6525226427
高小川 <gaoxiaochuan@hotmail.com>
2018-09-24 18:23:10 +0800

    change receive packet

--- include/ackhandler/received_packet_history.h
+++ include/ackhandler/received_packet_history.h
@@ -10,18 +10,22 @@
 namespace quicpp {
 namespace ackhandler {
 
-const int packet_interval_start = 1;
-const int packet_interval_end = 0;
+const int packet_interval_start = 0;
+const int packet_interval_end = 1;
 
 class received_packet_history {
 private:
-    std::list<std::pair<uint64_t, uint64_t>> ranges;
-    uint64_t lowest_in_received_packet_numbers;
+    std::list<std::pair<uint64_t, uint64_t>> _ranges;
+    uint64_t _lowest_in_received_packet_numbers;
 public:
+    received_packet_history();
     quicpp::base::error_t received_packet(uint64_t p);
     void delete_below(uint64_t p);
     std::vector<std::pair<uint64_t, uint64_t>> get_ack_ranges();
     std::pair<uint64_t, uint64_t> get_highest_ack_range();
+
+    std::list<std::pair<uint64_t, uint64_t>> &ranges();
+    uint64_t &lowest_in_received_packet_numbers();
 };
 
 }
--- src/ackhandler/received_packet_history.cc
+++ src/ackhandler/received_packet_history.cc
@@ -1,19 +1,24 @@
 #include "ackhandler/received_packet_history.h"
 #include "params.h"
+#include <iostream>
+#include <algorithm>
+
+quicpp::ackhandler::received_packet_history::received_packet_history()
+    : _lowest_in_received_packet_numbers(0) {}
 
 quicpp::base::error_t
 quicpp::ackhandler::received_packet_history::
 received_packet(uint64_t p) {
-    if (this->ranges.size() > quicpp::max_tracked_received_ack_ranges) {
+    if (this->_ranges.size() > quicpp::max_tracked_received_ack_ranges) {
         return quicpp::error::too_many_outstanding_received_ack_ranges;
     }
     
-    if (this->ranges.empty()) {
-        this->ranges.push_back(std::make_pair(p, p));
+    if (this->_ranges.empty()) {
+        this->_ranges.push_back(std::make_pair(p, p));
         return quicpp::error::success;
     }
 
-    for (auto el = this->ranges.rbegin(); el != this->ranges.rend(); el++) {
+    for (auto el = this->_ranges.rbegin(); el != this->_ranges.rend(); el++) {
         if (p >= std::get<quicpp::ackhandler::packet_interval_start>(*el) &&
             p <= std::get<quicpp::ackhandler::packet_interval_end>(*el)) {
             return quicpp::error::success;
@@ -30,37 +35,38 @@ received_packet(uint64_t p) {
         }
 
         if (range_extended) {
-            auto prev_itr = el + 1;
-            if (prev_itr != this->ranges.rend() &&
+            auto prev_itr = el;
+            prev_itr++;
+            if (prev_itr != this->_ranges.rend() &&
                 std::get<quicpp::ackhandler::packet_interval_end>(*prev_itr) + 1 ==
                 std::get<quicpp::ackhandler::packet_interval_start>(*el)) {
                 std::get<quicpp::ackhandler::packet_interval_end>(*prev_itr) =
                     std::get<quicpp::ackhandler::packet_interval_end>(*el);
-                this->ranges.erase(el.base());
+                this->_ranges.erase((++el).base());
                 return quicpp::error::success;
             }
             return quicpp::error::success;
         }
 
         if (p > std::get<quicpp::ackhandler::packet_interval_end>(*el)) {
-            this->ranges.insert((el - 1).base(), std::make_pair(p, p));
+            this->_ranges.insert(el.base(), std::make_pair(p, p));
             return quicpp::error::success;
         }
     }
 
-    this->ranges.insert(this->ranges.begin(), std::make_pair(p, p));
+    this->_ranges.push_front(std::make_pair(p, p));
     return quicpp::error::success;
 }
 
 void quicpp::ackhandler::received_packet_history::delete_below(uint64_t p) {
-    if (p <= this->lowest_in_received_packet_numbers) {
+    if (p <= this->_lowest_in_received_packet_numbers) {
         return;
     }
-    this->lowest_in_received_packet_numbers = p;
+    this->_lowest_in_received_packet_numbers = p;
 
-    auto next_elem = this->ranges.begin();
-    for (auto el = this->ranges.begin();
-         el != this->ranges.end();
+    auto next_elem = this->_ranges.begin();
+    for (auto el = this->_ranges.begin();
+         el != this->_ranges.end();
          el = next_elem) {
         next_elem = el;
         next_elem++;
@@ -70,7 +76,7 @@ void quicpp::ackhandler::received_packet_history::delete_below(uint64_t p) {
             std::get<quicpp::ackhandler::packet_interval_start>(*el) = p;
         }
         else if (std::get<quicpp::ackhandler::packet_interval_end>(*el) < p) {
-            this->ranges.erase(el);
+            this->_ranges.erase(el);
         }
         else {
             return;
@@ -80,17 +86,15 @@ void quicpp::ackhandler::received_packet_history::delete_below(uint64_t p) {
 
 std::vector<std::pair<uint64_t, uint64_t>>
 quicpp::ackhandler::received_packet_history::get_ack_ranges() {
-    if (this->ranges.empty()) {
+    if (this->_ranges.empty()) {
         return std::vector<std::pair<uint64_t, uint64_t>>();
     }
 
     std::vector<std::pair<uint64_t, uint64_t>> result;
-    for (auto el = this->ranges.rbegin();
-         el != this->ranges.rend();
+    for (auto el = this->_ranges.rbegin();
+         el != this->_ranges.rend();
          el++) {
-        using namespace quicpp::ackhandler;
-        result.push_back(std::make_pair(std::get<packet_interval_start>(*el),
-                                        std::get<packet_interval_end>(*el)));
+        result.push_back(*el);
     }
 
     return result;
@@ -98,8 +102,19 @@ quicpp::ackhandler::received_packet_history::get_ack_ranges() {
 
 std::pair<uint64_t, uint64_t>
 quicpp::ackhandler::received_packet_history::get_highest_ack_range() {
-    if (!this->ranges.empty()) {
-        return this->ranges.back();
+    if (!this->_ranges.empty()) {
+        return this->_ranges.back();
     }
     return std::make_pair(0UL, 0UL);
+}
+
+
+std::list<std::pair<uint64_t, uint64_t>> &
+quicpp::ackhandler::received_packet_history::ranges() {
+    return this->_ranges;
+}
+
+uint64_t &
+quicpp::ackhandler::received_packet_history::lowest_in_received_packet_numbers() {
+    return this->_lowest_in_received_packet_numbers;
 }
